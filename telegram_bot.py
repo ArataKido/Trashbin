d8df3fcc0e5db625226ba221292a50d231e33092
Arata <shahzod20030905@gmail.com>
2022-07-22 00:16:10 +0300

    C:/Program Files/Git/

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,21 @@
-import aiogram 
-import constant 
+from aiogram import Bot,types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from constant import API_KEY_TELEGRAM
 
+bot = Bot(token = API_KEY_TELEGRAM)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def echo_send(message: types.Message):
+	await message.reply("Привет!\nПокаа")
+	await message.answer(message.text)
+
+@dp.message_handler()
+async def echo_message(msg:types.Message):
+	await msg.reply_voice(msg.from_user.id, caption='уу сука' )
+
+
+
+
+executor.start_polling(dp, skip_updates=True)
